46abc5858171b6658165db1f40ca08e8f7a67aa1
Bill Glick <bglick@illinois.edu>
2021-07-22 16:30:04 -0500

    SECURITY-1309: Remove SUID bit for fusermount (#1)

--- manifests/vmware.pp
+++ manifests/vmware.pp
@@ -2,9 +2,19 @@
 #
 # Host customization if running on a VMware hypervisor
 #
+# @param files_remove_setuid
+#   Hash of file resource paramters that need setuid removed from them
+#
+# @param packages
+#   Array of packages to ensure installed for vmware
+#
+# @param services
+#   Array of services to ensure running for vmware
+#
 # @example
 #   include profile_virtual::vmware
 class profile_virtual::vmware (
+  Hash               $files_remove_setuid,
   Array[ String[1] ] $packages,
   Array[ String[1] ] $services,
 ) {
@@ -22,5 +32,11 @@ class profile_virtual::vmware (
     hasstatus  => true,
     hasrestart => true,
   }
+
+  # Remove setuid/setgid from key files
+  $file_remove_setuid_defaults = {
+    mode    => 'ug-s',
+  }
+  ensure_resources('file', $files_remove_setuid, $file_remove_setuid_defaults )
 
 }
